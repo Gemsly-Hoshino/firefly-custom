0de38d8eb7ac096a6e4ff15257bb93b4ec99f217
gemslycoder <gemsly@163.com>
2026-06-20 20:39:56 +0800

    update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,8 +1,45 @@
 import type { APIRoute } from "astro";
 
 const robotsTxt = `
+User-agent: Amazonbot
+Disallow: /
+
+User-agent: Applebot-Extended
+Disallow: /
+
+User-agent: CCBot
+Disallow: /
+
+User-agent: ClaudeBot
+Disallow: /
+
+User-agent: cohere-ai
+Disallow: /
+
+User-agent: Google-Extended
+Disallow: /
+
+User-agent: GPTBot
+Disallow: /
+
+User-agent: meta-externalagent
+Disallow: /
+
+User-agent: OAI-SearchBot
+Disallow: /
+
+User-agent: PerplexityBot
+Disallow: /
+
+User-agent: YouBot
+Disallow: /
+
 User-agent: *
-Disallow: /_astro/
+Allow: /$
+Allow: /posts/
+Allow: /sitemap-index.xml
+Allow: /sitemap-0.xml
+Disallow: /
 
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
