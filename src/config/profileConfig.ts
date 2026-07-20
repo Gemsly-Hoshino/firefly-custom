9d8004436a56368fb6b21e36401d762155a39fd9
Gemsly Hoshino <gemslyho@outlook.com>
2026-07-21 03:02:37 +0800

    chore(config): update profile link to Telegram live chat bot

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -22,9 +22,9 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-			name: "qq",
-			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com",
+			name: "telegram",
+			icon: "fa7-brands:telegram",
+			url: "https://t.me/GemslyLiveChatBot",
 			showName: false,
 		},
 		{
